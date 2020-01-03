a5df9df291e72397d3a551d60db17ce609c0420f
“halmuda” <halmuda6231@gmail.com>
2020-01-04 01:13:43 +0900

    add: boardlistページでエラーを表示するコンポーネントを追加

--- src/main.js
+++ src/main.js
@@ -3,10 +3,21 @@
 import Vue from 'vue'
 import 'es6-promise/auto'
 import App from './App'
+import ErrorBoundary from './ErrorBoundary.vue' // エラーを捕捉するコンポーネント
 import router from './router'
 import store from './store'
 
 Vue.config.productionTip = false
+Vue.config.performance = true // NODE_ENV == 'development'で測定有効化
+
+// ErrorBoundaryコンポーネントのインストール
+Vue.component(ErrorBoundary.name, ErrorBoundary)
+
+Vue.config.errorHandler = (err, vm, info) => {
+  console.error('errorHandler err:', err)
+  console.error('errorHandler vm:', vm)
+  console.error('errorHandler info:', info)
+}
 
 /* eslint-disable no-new */
 new Vue({
